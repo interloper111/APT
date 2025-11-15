a8151638892e2b6781b0f837215d7f16b10f744d
Interloper <81812373+interloper111@users.noreply.github.com>
2025-11-15 20:15:43 +0000

    Refactor Apt class to use new naming conventions

--- aptClass.py
+++ aptClass.py
@@ -1,4 +1,4 @@
-from quadraticFormula import quadraticFormula
+from quadratic_formula import quadratic_solve
 
 # Made class for the APTs, only input is initial vertical velocity
 # (since initial horizontal velocity is not required for any of the calculations I have not inlcuded it)
@@ -11,17 +11,18 @@ g= 9.81
 
 class Apt:
     
-    def __init__(self, initialVerticalVelocity):
+    def __init__(self, initial_vertical_velocity):
         
-        self.startTime = 0
-        self.velocity = initialVerticalVelocity
-        self.finalTime = quadraticFormula((-g/2), self.velocity, 1.2)[1] + self.startTime
+        self.start_time = 0
+        self.velocity = initial_vertical_velocity
+        self.final_time = quadratic_solve((-g/2), self.velocity, 1.2)[1] + self.start_time
         
-    def findPerfectStartTime(self, otherFinalTime):
+    def find_perfect_start_time(self, other_final_time):
         # Finds the perfect start time to hit the ground at the same time as apt1
-        self.startTime = otherFinalTime - self.finalTime
-        self.finalTime = quadraticFormula((-g/2), self.velocity, 1.2)[1] + self.startTime
+        self.start_time = other_final_time - self.final_time
+        self.final_time = quadratic_solve((-g/2), self.velocity, 1.2)[1] + self.start_time
 
                         
             
+
             
